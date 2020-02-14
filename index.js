e194d02fef11f6358c43eb56f502d3e19fff14b8
jenn-lang <jennifervallejos08@gmail>
2020-02-14 14:03:13 -0300

    anote algunas palbras para buscar

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//char[] caracteres = cadena.toCharArray();
+//caracteres[0] = Character.toUpperCase(caracteres[0]);//
+
+function mayuscula (palabra){
+ for (let i=0;i< )
+
+}
+ 
+// pasos para poder hacerlo:charat y toUpperCase
